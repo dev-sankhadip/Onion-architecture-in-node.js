8f1224a77523f58e7d1c99aaa3c5bc020350046b
dev-sankhadip <sankhadip.2000@gmail.com>
2020-07-13 20:15:01 +0530

    Student DA layer added

---
+++ src/DA/StudentDA.ts
@@ -0,0 +1,21 @@
+export class StudentDA {
+    async GetStudents() {
+
+    }
+
+    async GetStudet() {
+
+    }
+
+    async CreateStudent() {
+
+    }
+
+    async UpdateStudent() {
+
+    }
+
+    async DeleteStudent() {
+
+    }
+}
