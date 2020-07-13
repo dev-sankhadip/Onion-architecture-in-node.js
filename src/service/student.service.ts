f4eba77fd9b9d02815b5061aa7b0d9e4283469fc
dev-sankhadip <sankhadip.2000@gmail.com>
2020-07-13 20:15:57 +0530

    student service layer added

---
+++ src/service/student.service.ts
@@ -0,0 +1,25 @@
+import { StudentDA } from "../DA";
+
+export class StudentService {
+
+    constructor(private studentDA: StudentDA) { }
+    public async GetStudents() {
+
+    }
+
+    public async GetStudet() {
+
+    }
+
+    public async CreateStudent() {
+
+    }
+
+    public async UpdateStudent() {
+
+    }
+
+    public async DeleteStudent() {
+
+    }
+}
