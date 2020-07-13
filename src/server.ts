af6e5140335c17bd57b6a9cee12a359cc4fcbc20
dev-sankhadip <sankhadip.2000@gmail.com>
2020-07-13 20:18:11 +0530

    Server file configured

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { StudentDA } from './DA/index';
+import { StudentService } from './service/index';
+import { StudentRouter } from "./routes/index";
+
+
 dotenv.config();
 
 
 const app = express();
+const router = express.Router();
 
+app.use('/', router);
+
+StudentRouter(router, new StudentService(new StudentDA()));
 
 
 
