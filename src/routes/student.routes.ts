167c120d46bc14a849220d69adc35d105a7520f6
dev-sankhadip <sankhadip.2000@gmail.com>
2020-07-14 19:27:58 +0530

    Student Controller updated with service and DA

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -4,7 +4,13 @@ import { Router, Response, Request } from 'express';
 
 
 export const StudentRouter = (router: Router, service: StudentService): void => {
-    router.get('/', (req: Request, res: Response) => {
-        console.log(req);
+    router.get('/', async (req: Request, res: Response) => {
+        try {
+            const data = await service.GetStudents();
+            res.status(200).send(data)
+        }
+        catch (err) {
+            res.status(500).send({ "err": err })
+        }
     })
 }
