306c43ad53ad5d5dbf031590742b04f38531c17d
dev-sankhadip <sankhadip.2000@gmail.com>
2020-07-15 18:22:06 +0530

    public modifier added

--- src/DA/DBManager.ts
+++ src/DA/DBManager.ts
@@ -3,19 +3,7 @@ import { connection } from "./DBConnection";
 
 export class DBManager implements IDBManager {
 
-    ReadData(query: string, paramCollection: (string | number | boolean)[] = []): Promise<MySqlType> {
-        return new Promise((resolve, reject) => {
-            connection.query(query, paramCollection, (err, result) => {
-                if (err) {
-                    return reject(err)
-                }
-                console.log(typeof result)
-                return resolve(result);
-            })
-        })
-    }
-
-    InsertOrUpdateData(query: string, paramCollection: (string | number | boolean | undefined)[]): Promise<MySqlType> {
+    public ReadData(query: string, paramCollection: (string | number | boolean)[] = []): Promise<MySqlType> {
         return new Promise((resolve, reject) => {
             connection.query(query, paramCollection, (err, result) => {
                 if (err) {
@@ -26,7 +14,18 @@ export class DBManager implements IDBManager {
         })
     }
 
-    DeleteData(query: string, paramCollection: (string | number | boolean)[]): Promise<MySqlType> {
+    public InsertOrUpdateData(query: string, paramCollection: (string | number | boolean | undefined)[]): Promise<MySqlType> {
+        return new Promise((resolve, reject) => {
+            connection.query(query, paramCollection, (err, result) => {
+                if (err) {
+                    return reject(err)
+                }
+                return resolve(result);
+            })
+        })
+    }
+
+    public DeleteData(query: string, paramCollection: (string | number | boolean)[]): Promise<MySqlType> {
         return new Promise((resolve, reject) => {
             connection.query(query, paramCollection, (err, result) => {
                 if (err) {
